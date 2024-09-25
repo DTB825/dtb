a7c8a7b051d985838bb28ab07dc132ed1f53beed
Marclino Stanley <smarclino@gmail.com>
2024-09-25 11:52:49 +0200

    Change database configuration

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
   {
-    host: process.env.DATABASE_HOST, // Host
+    host:process.env.DATABASE_HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
